91ca4bc202c859be3ff6921a51f68846fd1791c9
David Israel Ruiz Cabrera <davidruiz@electritelecom.com>
2022-05-05 11:53:19 -0500

    pruebas exitosas

--- pruebas.js
+++ pruebas.js
@@ -1,25 +1,40 @@
 // Hacer la consulta al API de pokemon
-let totalOfPokemons = 3;
+let totalOfPokemons = 150;
 let allPokemones = [];
 var nuevoObjeto = 1;
 
 async function objectCreator(id){
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
     let pokemon = await response.json();
-    allPokemones.push(pokemon);
-    constructordeObjetos(allPokemones);
+    return pokemon
+    
+    
+    // console.log(allPokemones)
+    // constructordeObjetos(allPokemones);
 }
 
-for(let i=1; i<=totalOfPokemons; i++){
-    var array = objectCreator(i)
+async function pruebas(){
+    for(let i=1; i<=totalOfPokemons; i++){
+        allPokemones.push(await objectCreator(i));
+    }
+
+    for(let i=0; i<totalOfPokemons; i++){
+        console.log(allPokemones[i].id);
+    }
+    // AQUI      
 }
+
+pruebas();
+
+
+
 
 // array.then(resultado => {
 //     console.log(resultado)
 // });
 
-function constructordeObjetos(allPokemones){
-    return nuevoObjeto = allPokemones;
-}
+// function constructordeObjetos(allPokemones){
+//     return nuevoObjeto = allPokemones;
+// }
 
-console.log(nuevoObjeto)
+// console.log(nuevoObjeto)
